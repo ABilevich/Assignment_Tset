202f01f4e0571572b47d241ce24fa1c049c8d253
Andres Bilevich <andresbilevich@gmail.com>
2025-11-24 09:34:27 +0100

    chore: initial commit with base spring boot initializer project

---
+++ src/main/java/com/andres/tsetchallenge/TsetchallengeApplication.java
@@ -0,0 +1,13 @@
+package com.andres.tsetchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsetchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsetchallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andres/tsetchallenge/TsetchallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andres.tsetchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsetchallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
